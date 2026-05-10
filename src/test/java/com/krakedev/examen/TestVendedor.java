ad92134472f65b7ec8a95b825ff9c75aa725c7f7
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-05-09 20:10:05 -0500

    LMSM: Parte 1: implementacion de la clase TestVendedor

---
+++ src/test/java/com/krakedev/examen/TestVendedor.java
@@ -0,0 +1,15 @@
+package com.krakedev.examen;
+
+import com.krakedev.examen.entidades.Vendedor;
+
+public class TestVendedor {
+	public static void main(String[] args) {
+		Vendedor vendedor = new Vendedor("1234567890");
+		vendedor.setSueldoFijo(1000.0);
+		vendedor.setNumeroVentas(5);
+		vendedor.setComisionPorVenta(50.0);
+		vendedor.setTipo("V");
+
+		System.out.println(vendedor.toString());
+	}
+}
