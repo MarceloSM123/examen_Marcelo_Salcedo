af21b40a1b9b34d36ec37e440fa20dd0620c68e3
MarceloSM123 <136668018+MarceloSM123@users.noreply.github.com>
2026-05-09 20:26:12 -0500

    LMSM: Parte 1: Implementacion de la clase VendedorComisionJunitTest con las respectivas pruebas unitarias

---
+++ src/test/java/com/krakedev/examen/VendedorComisionJunitTest.java
@@ -0,0 +1,19 @@
+package com.krakedev.examen;
+
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.examen.entidades.VendedorComision;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VendedorComisionJunitTest {
+	 @Test
+	    public void testCalcularSueldo() {
+	        VendedorComision vendedor = new VendedorComision("1234567890");
+	        vendedor.setNumeroVentas(10);
+	        vendedor.setComisionPorVenta(50.0);
+	        double expected = 500.0;
+	        double actual = vendedor.calcularSueldo();
+	        assertEquals(expected, actual, 0.001);
+	    }
+}
